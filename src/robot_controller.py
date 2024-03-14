11ad8739284a22f3ab60fc1567164ceb59a31842
Mathew Salkeld <msalkeld1@sheffield.ac.uk>
2024-03-14 22:21:27 +0000

    added code for lidar scan

--- src/robot_controller.py
+++ src/robot_controller.py
@@ -4,6 +4,7 @@ import rospy
 import actionlib
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
 from tf.transformations import euler_from_quaternion
 from math import degrees
 import numpy as np
@@ -24,3 +25,19 @@ class RobotMove(object):
     def stop(self):
         self.set_move_cmd()
         self.publish()
+
+
+class RobotLaserScan(object):
+    def laserscan_cb(self, scan_data):
+        left_arc = scan_data.ranges[0:21]
+        right_arc = scan_data.ranges[-20:]
+        front_arc = np.array(left_arc[::-1] + right_arc[::-1])
+
+        self.min_distance = front_arc.min()
+        arc_angles = np.arrange(-20, 21)
+        self.closest_object_position = arc_angles[np.argmin(front_arc)]
+
+    def __init__(self):
+        self.min_distance = 0.0
+        self.closest_object_position = 0.0
+        self.subscriber = rospy.Subscriber('/scan', LaserScan, self.laserscan_cb)
